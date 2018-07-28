cc8fffdb4617acd5bc55731147d6f7c3fe122040
roger <1357852347@qq.com>
2018-07-28 09:06:45 +0800

    fork/join

--- demo/src/main/java/com/example/demo/sync/concurrent019/UseFuture.java
+++ demo/src/main/java/com/example/demo/sync/concurrent019/UseFuture.java
@@ -1,9 +1,6 @@
 package com.example.demo.sync.concurrent019;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.FutureTask;
+import java.util.concurrent.*;
 
 public class UseFuture implements Callable<String>{
 	private String para;
@@ -31,7 +28,7 @@ public class UseFuture implements Callable<String>{
 		//创建一个固定线程的线程池且线程数为1,
 		ExecutorService executor = Executors.newFixedThreadPool(1);
 		//这里提交任务future,则开启线程执行RealData的call()方法执行
-		executor.submit(future);
+		Future submit = executor.submit(future);
 		System.out.println("请求完毕");
 		try {
 			//这里可以做额外的数据操作，也就是主程序执行其他业务逻辑
@@ -41,6 +38,7 @@ public class UseFuture implements Callable<String>{
 		}
 		//调用获取数据方法,如果call()方法没有执行完成,则依然会进行等待
 		System.out.println("数据：" + future.get());
+		System.out.println("数据：" + submit.get());
 		executor.shutdown();
 	}
 
