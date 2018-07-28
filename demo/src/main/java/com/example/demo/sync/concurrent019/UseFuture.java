dd861ef8e23e33f2325818426b0b20cf722522ec
roger <1357852347@qq.com>
2018-07-28 15:07:31 +0800

    mq

--- demo/src/main/java/com/example/demo/sync/concurrent019/UseFuture.java
+++ demo/src/main/java/com/example/demo/sync/concurrent019/UseFuture.java
@@ -16,15 +16,14 @@ public class UseFuture implements Callable<String>{
 	public String call() throws Exception {
 		//模拟执行耗时
 		Thread.sleep(3000);
-		String result = this.para + "处理完成";
-		return result;
+		return this.para + "处理完成";
 	}
 	
 	//主控制函数
 	public static void main(String[] args) throws Exception {
 		String queryStr = "query";
 		//构造FutureTask，并且传入需要真正进行业务逻辑处理的类,该类一定是实现了Callable接口的类
-		FutureTask<String> future = new FutureTask<String>(new UseFuture(queryStr));
+		FutureTask<String> future = new FutureTask<>(new UseFuture(queryStr));
 		//创建一个固定线程的线程池且线程数为1,
 		ExecutorService executor = Executors.newFixedThreadPool(1);
 		//这里提交任务future,则开启线程执行RealData的call()方法执行
